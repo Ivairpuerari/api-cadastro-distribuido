28d46c10dfaf69b089ef32ef2f8d353aa8989408
Ivair Puerari <ivaair@hotmail.com>
2019-11-15 02:19:30 -0300

    testes de metodos e urls

---
+++ model.py
@@ -0,0 +1,5 @@
+class Inscrito:
+    def __init__(self, nome, email, matricula):
+        self.nome = nome
+        self.email = email
+        self.matricula = matricula
--- server.py
+++ server.py
@@ -1,44 +1,56 @@
-from bottle import request, response
+from bottle import request, response, run
 from bottle import post, get, put, delete
+
+import model
 
 #import json
 #import threading
 #import time
 import sys
 
+SIZE_MEMORIA = 5
+
+API_BASE_URL = 'http://localhost:'
+
+MAXIMO_DE_PORTAS = 50010
+
+MINIMO_DE_PORTAS = 50000
+
+PORTA_API= 5000 + int(sys.argv[1])
+
+NODOS_VIZINHOS = [PORTA_API]
+
+@get('/alive')
+def alive():
+    [request.get(API_BASE_URL + str(porta) + '/' ) for porta in range(MINIMO_DE_PORTAS, MAXIMO_DE_PORTAS, 1) if porta != PORTA_API]
+
+@get('')
+
 @post('/inscricao')
 def inscricao():
-    """
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    if check_login(username, password):
-        return "<p>Your login information was correct.</p>"
-    else:
-        return "<p>Login failed.</p>"
-    """
+    
+    inscrito = model.Inscrito(  request.query.nome,
+                                request.query.email,
+                                request.query.matricula)
+
+    print(inscrito.matricula)
+    #if True:
+    #    return response(status=200)
+    #else: 
+    #    return response(status=204)
+     
 
 @get('/todos-inscritos') # or @route('/login')
 def todos_inscritos():
-    return '''
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    return "Todos os inscritos"
 
 @get('/inscrito') # or @route('/login')
 def inscrito():
-    return '''
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    return "Um inscrito! "  
 
 
-bottle.run(host='localhost', port=int(sys.argv[1]))
+
+run(host='localhost', port=PORTA_API, debug= True)
 """
 peers = sys.argv[2:]
 
